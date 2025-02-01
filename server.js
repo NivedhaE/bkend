1e4ef8fb677bf87a18de8e61f399e02b05ff405b
NivedhaE <nivedhaezhilarasan@gmail.com>
2025-02-01 15:52:30 +0530

    mmm

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors=require('cors');
 app.use(express.json());//middleware
 app.use(cors())
 
-const routerurl=require('./router/router') //db+router
+const routerurl=require('./router') //db+router
 app.use('/app',routerurl);
 //mongodb connection
 if(mongoose.connect('mongodb+srv://db-nivi:Mininivi8499@cluster0.wmxjq.mongodb.net/registration?retryWrites=true&w=majority&appName=Cluster0'))
